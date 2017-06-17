b096143e52d0bc6a238cfee7049b13c522f1d2e0
mzappitello <mzappitello@gmail.com>
2017-06-17 15:52:10 +0000

    Some cleanup to main event loop
    * Add some comments for clariity
    * Add desired messaging statements on threshold crossings
    * Increment next display time from previous display time rather than now.

--- src/__main__.py
+++ src/__main__.py
@@ -7,38 +7,62 @@ from log_utils import *
 
 if __name__ == '__main__':
     log_filename = os.path.join(
-            # 'var', 'log', 'nginx', 'access.log'
+            # '/', 'var', 'log', 'nginx', 'access.log')
             os.path.dirname(__file__), '..', 'test_data', 'access.log')
 
+    # Create a tail of the log filename, a parser to go over it and a timestamp.
     logger = LogTail(log_filename)
     parser = LogParser()
-
-    threshold = 20
-    threshold_queue = queue.Queue()
-    display_list = [ ] 
-
     now = datetime.datetime.now()
+
+    # Create a threshold queue that will hold LogItems in FIFO order. Items will
+    # be popped off the top if they are from before now - threshold_delta (aka
+    # two minutes ago). Keep a flag on the current threshold status.
+    threshold_delta = datetime.timedelta(days=2)
+    threshold_queue = queue.Queue()
+    threshold = 20
+    above_threshold = False
+
+    # Create a display list that will hold LogItems. Everytime now >
+    # next_display, show some stats on display list, clear the list, and set
+    # next_display to 10 seconds from the last one.
     display_delta = datetime.timedelta(seconds=10)
     next_display = now + display_delta
-    threshold_delta = datetime.timedelta(minutes=2)
+    display_list = [ ] 
 
     while True:
         now = datetime.datetime.now()
-        threshold_time = now - threshold_delta
 
-        while not threshold_queue.empty() and threshold_queue.queue[0].time < threshold_time:
+        threshold_time = now - threshold_delta
+        while not threshold_queue.empty() and \
+                threshold_queue.queue[0].time < threshold_time:
             _ = threshold_queue.get()
 
-        if len(threshold_queue.queue) > threshold:
-            print("OH NO TOO MANY HITS!")
+        threshold_size = len(threshold_queue.queue)
+        if threshold_size > threshold:
+            if not above_threshold:
+                above_threshold = True
+                message =  \
+                        "High traffic generated an alert - hits = %s " \
+                        "triggered at %s" % \
+                        (threshold_size, now.strftime("%A, %d. %B %Y %I:%M%p"))
+                print(message)
+        else:
+            if above_threshold:
+                above_threshold = False
+                message = \
+                        "Traffic fell back below threshold - hits = %s " \
+                        "triggered at %s" % \
+                        (threshold_size, now.strftime("%A, %d. %B %Y %I:%M%p"))
+                print(message)
 
         if now > next_display:
-            next_display = now + display_delta
-            print("display time!")
+            next_display = next_display + display_delta
+            print("total log items: %s" % len(display_list))
 
             # do some processing for display
             for i, item in enumerate(display_list):
-                print(item.request)
+                a = "a"
 
             display_list = [ ]
 
